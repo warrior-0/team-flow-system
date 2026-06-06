af6dfbb9e8cd815890fde48e57f69f54c96a64f5
warrior-0 <bodla0330@gmail.com>
2026-06-06 17:01:55 +0900

    Delete src directory

--- src/components/project/StatusBadge.jsx
+++
@@ -1,5 +0,0 @@
-import { getStatusLabel } from '../../utils/taskHelpers';
-
-export default function StatusBadge({ status }) {
-  return <span className={`status-badge status-${status}`}>{getStatusLabel(status)}</span>;
-}
